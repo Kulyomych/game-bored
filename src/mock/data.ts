b53c579059889b1a39883c2bc16805d8a975ee42
Denis <den.xrustalev17@yandex.ru>
2026-03-09 01:51:51 +0300

    Заменить json на ts

---
+++ src/mock/data.ts
@@ -0,0 +1,32 @@
+import type { Game } from "../types/game";
+
+export const cardData: Game[] = [
+  {
+    id: "c9d0e1f2-a3b4-5678-cdef-789012345678",
+    title: "Монополия",
+    img: "https://ic.pics.livejournal.com/sergofan_prok/31949486/37024/37024_original.jpg",
+    oldPrice: 3299,
+    price: 2499,
+  },
+  {
+    id: "d0e1f2a3-b4c5-6789-defa-890123456789",
+    title: "Мемо",
+    img: "https://ic.pics.livejournal.com/sergofan_prok/31949486/37024/37024_original.jpg",
+    oldPrice: 999,
+    price: 699,
+  },
+  {
+    id: "e1f2a3b4-c5d6-7890-efab-901234567890",
+    title: "Бункер",
+    img: "https://ic.pics.livejournal.com/sergofan_prok/31949486/37024/37024_original.jpg",
+    oldPrice: 2499,
+    price: 1799,
+  },
+  {
+    id: "f2a3b4c5-d6e7-8901-fabc-012345678901",
+    title: "Элиас",
+    img: "https://ic.pics.livejournal.com/sergofan_prok/31949486/37024/37024_original.jpg",
+    oldPrice: 1999,
+    price: 1499,
+  },
+];
