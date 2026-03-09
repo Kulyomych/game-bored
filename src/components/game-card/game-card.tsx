373c8e6115a0d6f351b533d349597e331e305209
Denis <den.xrustalev17@yandex.ru>
2026-03-09 22:56:43 +0300

    добавить стили к карточкам

--- src/components/game-card/game-card.tsx
+++ src/components/game-card/game-card.tsx
@@ -1,28 +1,23 @@
 import type React from "react";
 import styles from "./game-card.module.css";
+import type { Game } from "../../types/game";
 
 type GameCardProps = {
-  id?: string;
-  img?: string;
-  title: string;
-  price?: number;
-  oldPrice?: number;
+  data: Game;
 };
 
-export const GameCard: React.FC<GameCardProps> = (props) => {
-  const { id, img, title, price, oldPrice } = props;
+export const GameCard: React.FC<GameCardProps> = ({ data }) => {
   return (
     <article className={styles.card}>
       <div className={styles.content}>
-        <div className={styles.imageBlock}></div>
-        <div className={styles.buttonBlock}>
-          <div className={styles.info}>
-            <h3>{title}</h3>
-            <div className={styles.price}>
-              <span>2500 р.</span>1790 р.<span></span>
-            </div>
-          </div>
-          <button className={styles.cardButton}>Добавить в корзину</button>
+        <div className={styles.imageBlock}>
+          <img src={data.img} alt={data.title} loading="lazy" />
+        </div>
+
+        <h3>{data.title}</h3>
+        <div className={styles.price}>
+          <span className={styles.oldPrice}>{data.oldPrice} р.</span>
+          <span className={styles.newPrice}>{data.price} р.</span>
         </div>
       </div>
     </article>
