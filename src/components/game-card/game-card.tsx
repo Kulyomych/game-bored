ac313b316527847f4154ee3b66c62eda567251b0
Denis <den.xrustalev17@yandex.ru>
2026-03-12 22:34:01 +0300

    добавить стили к карточкам

--- src/components/game-card/game-card.tsx
+++ src/components/game-card/game-card.tsx
@@ -1,23 +1,33 @@
-import type React from "react";
+import React from "react";
 import styles from "./game-card.module.css";
 import type { Game } from "../../types/game";
 
 type GameCardProps = {
   data: Game;
+  className?: string;
+  onSelect?: () => void;
 };
 
-export const GameCard: React.FC<GameCardProps> = ({ data }) => {
+export const GameCard: React.FC<GameCardProps> = ({
+  data,
+  className,
+  onSelect,
+}) => {
+  const { title, oldPrice, price, img } = data;
   return (
-    <article className={styles.card}>
+    <article className={`${styles.card} ${className}`} onClick={onSelect}>
       <div className={styles.content}>
-        <div className={styles.imageBlock}>
-          <img src={data.img} alt={data.title} loading="lazy" />
+        <div className={styles.image}>
+          <img src={img} alt={title} loading="lazy" />
         </div>
 
-        <h3>{data.title}</h3>
-        <div className={styles.price}>
-          <span className={styles.oldPrice}>{data.oldPrice} р.</span>
-          <span className={styles.newPrice}>{data.price} р.</span>
+        <div className={styles.info}>
+          <h3 className={styles.title}>{title}</h3>
+          <div className={styles.prices}>
+            <span className={styles.oldPrice}>{oldPrice} р.</span>
+            <span className={styles.newPrice}>{price} р.</span>
+            <div />
+          </div>
         </div>
       </div>
     </article>
